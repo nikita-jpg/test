13f6cd45df7954fe33f1a17fad1063950837619d
John Doe <johndoe@example.com>
2021-01-07 16:05:37 +0300

    Updates

---
+++ precache-manifest.e322f7e091788d47414c1a4e33008b5b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29de816048c1176a7ab07cac2b52b78b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "82b3a20d478a2d20c345",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "9f173754e3216e7e3ad0",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "82b3a20d478a2d20c345",
+    "url": "./static/js/2.f6f49ff7.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.f6f49ff7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f173754e3216e7e3ad0",
+    "url": "./static/js/main.f22d3716.chunk.js"
+  },
+  {
+    "revision": "066e1fe302588d5b0de2",
+    "url": "./static/js/runtime-main.560b22e6.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);

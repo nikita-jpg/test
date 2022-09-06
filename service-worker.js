7a1d17570eae8196665db2e4d1dc2a90d81bc70a
nikit <nicitazaletin@yandex.ru>
2022-09-06 17:18:02 +0300

    test_commit_1

--- service-worker.js
+++ service-worker.js
@@ -34,6 +34,5 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
-  
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
